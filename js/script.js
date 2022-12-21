f2d30ca98a997347e2b5cc5eed2b612638f613e2
lukejans <91230392+lukejans@users.noreply.github.com>
2022-12-21 16:58:59 -0500

    Initial operations

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// ADD //
+function add(a, b) {
+  return a + b;
+}
+
+// SUBTRACT //
+function subtract(a, b) {
+  return a - b;
+}
+
+// MULTIPLY //
+function multiply(a, b) {
+  return a * b;
+}
+
+// DIVIDE //
+function divide(a, b) {
+  return a / b;
+}
