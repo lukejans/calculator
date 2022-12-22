785a3dca0c69f8031782cf7dc58351b84e231da0
lukejans <91230392+lukejans@users.noreply.github.com>
2022-12-21 19:43:48 -0500

    Operate multiple value inputs from an array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//  EVENT LISTENERS //
+
 // ADD //
 function add(a, b) {
   return a + b;
@@ -17,3 +19,44 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+// OPERATE //
+function operate(numbers, operators) {
+  // multiplication and division
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] == '*') {
+      numbers[i] = multiply(numbers[i], numbers[i + 1]);
+      numbers.splice(i + 1, 1);
+      operators.splice(i, 1);
+      i--;
+    } else if (operators[i] == '/') {
+      numbers[i] = divide(numbers[i], numbers[i + 1]);
+      numbers.splice(i + 1, 1);
+      operators.splice(i, 1);
+      i--;
+    }
+  }
+  // addition and subtraction
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] == '+') {
+      numbers[i] = add(numbers[i], numbers[i + 1]);
+      numbers.splice(i + 1, 1);
+      operators.splice(i, 1);
+      i--;
+    } else if (operators[i] == '-') {
+      numbers[i] = subtract(numbers[i], numbers[i + 1]);
+      numbers.splice(i + 1, 1);
+      operators.splice(i, 1);
+      i--;
+    }
+  }
+
+  return numbers[0];
+}
+// Example usage:
+const numbers = [1, 2, 3, 4, 5];
+const operators = ['+', '-', '*', '/'];
+console.log(operate(numbers, operators));
+
+// CLEAR //
+function clear() {}
