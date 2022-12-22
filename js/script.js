e64d93a5bdba3f93de7ae4ed0f47e4a004242271
lukejans <91230392+lukejans@users.noreply.github.com>
2022-12-21 21:11:03 -0500

    Button Setup

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 //  EVENT LISTENERS //
+const equalsBtn = document.getElementById('equals');
+
+const zeroBtn = document.getElementById('zero');
+const oneBtn = document.getElementById('one');
+const twoBtn = document.getElementById('two');
+const threeBtn = document.getElementById('three');
+const fourBtn = document.getElementById('four');
+const fiveBtn = document.getElementById('five');
+const sixBtn = document.getElementById('six');
+const sevenBtn = document.getElementById('seven');
+const eightBtn = document.getElementById('eight');
+const nineBtn = document.getElementById('nine');
+
+const screenOutput = document.getElementById('dis-in-out');
+
+// EQUALS
+let equals = '';
 
 // ADD //
 function add(a, b) {
@@ -50,7 +67,8 @@ function operate(numbers, operators) {
       i--;
     }
   }
-
+  // EQUALS //
+  equals = numbers[0].toFixed(2);
   return numbers[0];
 }
 // Example usage:
@@ -60,3 +78,45 @@ console.log(operate(numbers, operators));
 
 // CLEAR //
 function clear() {}
+
+// PRINT EQUALS //
+equalsBtn.addEventListener('click', function () {
+  console.log(equals);
+  screenOutput.textContent = equals;
+  sumHistory = equals;
+  equals = '';
+});
+
+// DIGITS //
+zeroBtn.addEventListener('click', function () {
+  console.log('0');
+});
+oneBtn.addEventListener('click', function () {
+  console.log('1');
+});
+twoBtn.addEventListener('click', function () {
+  console.log('2');
+});
+threeBtn.addEventListener('click', function () {
+  console.log('3');
+});
+fourBtn.addEventListener('click', function () {
+  console.log('4');
+});
+fiveBtn.addEventListener('click', function () {
+  console.log('5');
+});
+sixBtn.addEventListener('click', function () {
+  console.log('6');
+});
+sevenBtn.addEventListener('click', function () {
+  console.log('7');
+});
+eightBtn.addEventListener('click', function () {
+  console.log('8');
+});
+nineBtn.addEventListener('click', function () {
+  console.log('9');
+});
+
+// DISPLAY CURRENT INPUT //
